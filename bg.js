cda1677bd9b2250d1a937f8df676fe5ab70876a8
Sunwha <ohsw17@naver.com>
2020-10-17 23:35:42 +0900

    background test

--- bg.js
+++ bg.js
@@ -1,13 +1,14 @@
 const body = document.querySelector("body");
+const wrapper = body.querySelector(".wrapper");
 
+const API_KEY = "J4crT2JyXxsnQ7Rloi7igShD_fkCeb4xNSONvzHXVt4";
 const IMG_NUMBER = 3;
 
 function paintImage(imgNumber){
   const image = new Image();
   image.src = `/images/${imgNumber+1}.jpg`;
-  image.classList.add("bgImage");
-  body.appendChild(image);
-}
+  // image.classList.add("bgImage");
+  wrapper.style.backgroundImage = image.src;
 
 function genRandom(){
   const number = Math.floor(Math.random() * IMG_NUMBER);
