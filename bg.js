1e8dae2d5f704abd96f8be602786278dc6df12ce
Sunwha <ohsw17@naver.com>
2020-10-18 00:31:27 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,7 +1,7 @@
 const body = document.querySelector("body");
 const wrapper = body.querySelector(".wrapper");
 
-const API_KEY = "J4crT2JyXxsnQ7Rloi7igShD_fkCeb4xNSONvzHXVt4";
+// const API_KEY = "J4crT2JyXxsnQ7Rloi7igShD_fkCeb4xNSONvzHXVt4";
 const IMG_NUMBER = 3;
 
 function paintImage(imgNumber){
